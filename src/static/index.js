21f07911cf1fc0c098e4e44b325490e71a92d408
joey-squla <joey@squla.com>
2019-07-17 17:02:53 +0200

    Connected data up to plot

--- src/static/index.js
+++ src/static/index.js
@@ -2,13 +2,39 @@ import Plotly from "plotly.js-dist";
 
 class App{
     constructor(){
-        console.log(CONFIG);
-        Plotly.plot("gd", [{ x: [0, 1], y: [0, 1] }]);
-        console.log("App initialized");
-
-
-        this.postData().then(data => console.log(JSON.stringify(data))) // JSON-string from `response.json()` call
+        this.postData().then(data => this.displayData(data)) // JSON-string from `response.json()` call
             .catch(error => console.error(error));
+    }
+
+    displayData(data){
+        let dataList = JSON.parse(data);
+
+        let dataIndexList = [];
+        for(var i=0;i<dataList.length;i++){
+            dataIndexList.push(i);
+        }
+
+        var trace1 = {
+            x: dataList,
+            y: dataIndexList,
+            mode: 'markers',
+            type: 'scatter',
+            name: 'Team A',
+            text: ['A-1', 'A-2', 'A-3', 'A-4', 'A-5'],
+            marker: { size: 12 }
+        };
+
+        var layout = {
+            xaxis: {
+                range: [ 0, 44]
+            },
+            yaxis: {
+                range: [0, 44]
+            },
+            title:'Data Labels Hover'
+        };
+        Plotly.newPlot('gd', [trace1], layout);
+        //Plotly.plot("gd", [{ x: [0, 1], y: [0, 1] }]);
     }
 
     postData(url = '', data = {}) {
